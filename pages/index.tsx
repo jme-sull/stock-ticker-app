06b6994f076cf31c744f97fd5c2db52f06ed4939
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-02-19 12:35:20 -0500

    adds state to handle errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,44 @@
-import { Grid } from "@material-ui/core";
+import { Grid, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { NextPage } from "next";
+import { useState } from "react";
 import Layout from "../components /Layout";
 
 const useStyles = makeStyles({
   root: {
-    margin: 10,
+    flexGrow: 1,
+    margin: 20,
+  },
+  item: {
+    height: "100vh",
+    padding: 10,
   },
 });
 
 const HomePage: NextPage = () => {
   const classes = useStyles();
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState("");
   return (
     <Layout>
       <div className={classes.root}>
-        <Grid container spacing={3}>
+        <Grid container spacing={0}>
           <Grid item xs={12} sm={6}>
-            <h2>Box #1</h2>
+            <div className={classes.item}>
+              Enter Ticker Symbol
+              <form>
+                <TextField
+                  error={error}
+                  defaultValue="TICKER"
+                  helperText={errorText}
+                  variant="outlined"
+                />
+              </form>
+            </div>
           </Grid>
+          {/* <Divider orientation="vertical" flexItem /> */}
           <Grid item xs={12} sm={6}>
-            <h2>Box #2</h2>
+            <div className={classes.item}>Box #2</div>
           </Grid>
         </Grid>
       </div>
