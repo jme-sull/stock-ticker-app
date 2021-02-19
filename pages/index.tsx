698a8e3a99043a9f2b574880cc65d1596adcccec
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-02-19 13:17:34 -0500

    adds material icons, sets icon, sets up state for stock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Grid, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import ArrowForwardOutlinedIcon from "@material-ui/icons/ArrowForward";
 import { NextPage } from "next";
 import { useState } from "react";
 import Layout from "../components /Layout";
@@ -7,11 +8,26 @@ import Layout from "../components /Layout";
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
-    margin: 20,
+    margin: 30,
   },
   item: {
     height: "100vh",
     padding: 10,
+  },
+  formItem: {
+    width: "70%",
+  },
+  icon: {
+    backgroundColor: "#d7d7d8",
+    color: "#878787",
+    height: "100%",
+    borderRadius: 3,
+    borderColor: "#969696",
+    borderWidth: "10px",
+    cursor: "pointer",
+    fontSize: 45,
+    align: "center",
+    marginLeft: "10px",
   },
 });
 
@@ -19,21 +35,26 @@ const HomePage: NextPage = () => {
   const classes = useStyles();
   const [error, setError] = useState(false);
   const [errorText, setErrorText] = useState("");
+  const [stock, setStock] = useState({});
+  const [fetchedStock, setFetchedStock] = useState(false);
   return (
     <Layout>
       <div className={classes.root}>
         <Grid container spacing={0}>
           <Grid item xs={12} sm={6}>
             <div className={classes.item}>
-              Enter Ticker Symbol
+              <h1 style={{ fontSize: "18px" }}>Enter Ticker Symbol</h1>
               <form>
                 <TextField
+                  className={classes.formItem}
                   error={error}
-                  defaultValue="TICKER"
+                  label="TICKER"
                   helperText={errorText}
                   variant="outlined"
                 />
+                <ArrowForwardOutlinedIcon className={classes.icon} />
               </form>
+              {fetchedStock && <div>THIS SHOULD BE INVISABLE</div>}
             </div>
           </Grid>
           {/* <Divider orientation="vertical" flexItem /> */}
