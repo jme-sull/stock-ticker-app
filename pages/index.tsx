029442b88d3e355fadc0d02112b6844ea59b68db
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-02-23 06:57:16 -0500

    adds logic for passing in data to graph

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { NextPage } from "next";
+import Graph from "../components /Graph";
 import Input from "../components /Input";
 import Layout from "../components /Layout";
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
-    margin: 30,
   },
   item: {
     height: "100vh",
-    padding: 10,
   },
 });
 
@@ -19,14 +18,16 @@ const HomePage: NextPage = () => {
   const classes = useStyles();
   return (
     <Layout>
-      <Grid className={classes.root} container spacing={3}>
-        <Grid className={classes.item} item xs={12} sm={6}>
-          <Input />
+      <div style={{ padding: "3%" }}>
+        <Grid className={classes.root} container spacing={0}>
+          <Grid className={classes.item} item xs={12} sm={6}>
+            <Input />
+          </Grid>
+          <Grid className={classes.item} item xs={12} sm={6}>
+            <Graph label={"Apple"} />
+          </Grid>
         </Grid>
-        <Grid className={classes.item} item xs={12} sm={6}>
-          <div>GRAPH GOES HERE</div>
-        </Grid>
-      </Grid>
+      </div>
     </Layout>
   );
 };
