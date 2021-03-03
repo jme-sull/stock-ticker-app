e429d86b1bc314d0833059c05b24459901aeba5c
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-03-03 10:08:17 -0500

    updates title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <title>NDVR App Challenge</title>
+        <title>Stock Ticker App</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
